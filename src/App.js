c2469cc58af65501e15c1d28dd3d802202537ee8
wilson evangelista <wilson.evangelista22@gmail.com>
2022-03-31 21:56:46 -0300

    add o recurso toastify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Routes from './routes'
 import './style.css'
+import 'react-toastify/dist/ReactToastify.css'
+import {ToastContainer} from 'react-toastify'
 function App() {
   return (
     <div className='app'>
       <Routes/>
+      <ToastContainer autoClose={3000}/>
     </div>
 
   
