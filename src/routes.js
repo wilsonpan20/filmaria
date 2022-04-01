6bdf9f0abc301b44a1206f391e1bc52192245caa
wilson evangelista <wilson.evangelista22@gmail.com>
2022-03-31 22:15:30 -0300

    criando pagina de não encontrado

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import Filme from './pages/Filme'
 import Favoritos from './pages/Favoritos'
+import Erro from './pages/Erro'
 
 const Routes =()=> {
     return(
@@ -13,6 +14,8 @@ const Routes =()=> {
             <Route  exact path="/" component={Home}/>
             <Route  exact path="/filme/:id" component={Filme}/>
             <Route exact path="/favoritos" component={Favoritos}/>
+            <Route exact path="*" component={Erro}/>
+
         </Switch>
         </BrowserRouter>
     )
