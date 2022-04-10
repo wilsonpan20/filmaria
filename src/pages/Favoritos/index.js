37f811a46f478f64e864cde66604d97ba5d6274b
wilson evangelista <wilson.evangelista22@gmail.com>
2022-04-10 09:29:58 -0300

    Ajustado mobileFirst

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -24,7 +24,7 @@ function Favoritos(){
     return(
         <div id="meus-filmes">
             <h1> Meus Filmes</h1>
-            {filmes.length === 0 && <span>Você não possui nenhum filme salvo :(</span>}
+            {!filmes.length  && <span>Você não possui nenhum filme salvo :(</span>}
              <ul>
                  {filmes.map((item)=>{
                      return(
